6dd86814d1e8afdba1f2546e515ba834ea22cdee
erduotong <3135351980@qq.com>
2024-11-15 23:40:33 +0800

    locals

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,11 @@
+use serde::{Deserialize, Serialize};
+use serde_json::to_string_pretty;
 use std::collections::VecDeque;
+use std::fs::File;
+use std::io::Write;
 use std::sync::Mutex;
-use serde::{Serialize, Deserialize};
+static DATA_STORAGE_TARGET: &str = "just-tauri-todo.json";
+
 #[derive(Clone, Serialize, Deserialize)]
 struct TodoItem {
     text: String,
@@ -14,10 +19,39 @@ struct TodoList {
 
 impl TodoList {
     fn add(&mut self, content: String) {
-        self.items.push_front(TodoItem { text: content, done: false });
+        self.items.push_front(TodoItem {
+            text: content,
+            done: false,
+        });
+        self.save_to_file().unwrap();
     }
-    fn remove(&mut self, index: i64) {
+    fn remove(&mut self, index: usize) {
         self.items.remove(index as usize).unwrap();
+        self.save_to_file().unwrap();
+    }
+    fn update_done_status(&mut self, index: usize, done: bool) {
+        self.items[index].done = done;
+        self.save_to_file().unwrap();
+    }
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let serialized = to_string_pretty(&self.items)?;
+        let mut file = File::create(DATA_STORAGE_TARGET)?;
+        file.write_all(serialized.as_bytes())?;
+        Ok(())
+    }
+    fn load_from_file(&mut self) -> std::io::Result<()> {
+        if !std::path::Path::new(DATA_STORAGE_TARGET).exists() {
+            return Ok(());
+        }
+        let file = File::open(DATA_STORAGE_TARGET)?;
+        let items: VecDeque<TodoItem> = serde_json::from_reader(file)?;
+        self.items = items;
+        Ok(())
+    }
+    fn new() -> Self {
+        TodoList {
+            items: VecDeque::new(),
+        }
     }
 }
 
@@ -29,22 +63,43 @@ fn add_todo(content: String, state: tauri::State<TodoState>) -> VecDeque<TodoIte
     state.add(content);
     state.items.clone()
 }
+
 #[tauri::command]
-fn remove_todo(index: i64, state: tauri::State<TodoState>) {
+fn remove_todo(index: usize, state: tauri::State<TodoState>) -> VecDeque<TodoItem> {
     println!("Removing todo at index: {}", index);
     let mut state = state.0.lock().unwrap();
     state.remove(index);
     state.items.clone()
 }
+#[tauri::command]
+fn update_done_status(
+    index: usize,
+    done: bool,
+    state: tauri::State<TodoState>,
+) -> VecDeque<TodoItem> {
+    println!("Updating todo at index: {}", index);
+    let mut state = state.0.lock().unwrap();
+    state.update_done_status(index, done);
+    state.items.clone()
+}
 
+#[tauri::command]
+fn init(state: tauri::State<TodoState>) -> VecDeque<TodoItem> {
+    let mut state = state.0.lock().unwrap();
+    state.load_from_file().unwrap();
+    state.items.clone()
+}
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .manage(TodoState(Mutex::new(TodoList {
-            items: VecDeque::new(),
-        })))
-        .invoke_handler(tauri::generate_handler![add_todo,remove_todo])
+        .manage(TodoState(Mutex::new(TodoList::new())))
+        .invoke_handler(tauri::generate_handler![
+            add_todo,
+            remove_todo,
+            update_done_status,
+            init
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {invoke} from "@tauri-apps/api/core";
-//
-// const greetMsg = ref("");
-// const name = ref("");
 
-// async function greet() {
-//   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-//   greetMsg.value = await invoke("greet", { name: name.value });
-// }
 interface todoItem {
   text: string;
   done: boolean;
@@ -17,24 +10,7 @@ interface todoItem {
 type todoList = todoItem[]
 
 // @ts-ignore unused
-const todoList = ref<todoList>([
-  {
-    text: "Learn Vue 3",
-    done: false
-  },
-  {
-    text: "Build a Tauri app",
-    done: false
-  },
-  {
-    text: "Write documentation",
-    done: false
-  },
-  {
-    text: "Test the application",
-    done: true
-  }
-])
+const todoList = ref<todoList>([])
 const todoContentInput = ref("")
 
 async function removeTodo(index: number) {
@@ -43,27 +19,34 @@ async function removeTodo(index: number) {
 
 // @ts-ignore
 async function addTodo() {
-  if (todoContentInput.value === "") {
+  if (todoContentInput.value.trim() === "" ) {
     return
   }
-  const res = await invoke("add_todo", {content: todoContentInput.value}) as todoList
-  console.log(res);
-  todoList.value = res;
+  todoList.value = await invoke("add_todo", {content: todoContentInput.value}) as todoList
 }
+
+async function updateTodoDoneStatus(index: number, done: boolean) {
+  todoList.value = await invoke("update_done_status", {index: index, done: done}) as todoList
+}
+onMounted(async () => {
+  todoList.value = await invoke("init")
+})
 </script>
 
 <template>
   <div class="flex-col max-w-[300px] mx-auto mt-16">
-    <h1 class="text-3xl">Just Tauri Todo</h1>
-    <span>a simple todo app</span>
-    <div class="mt-3 flex-row">
-      <input type="text" class="w-[80vw] max-w-[280px] rounded pl-1 pr-1" placeholder="Press Enter to add a new todo"
+    <h1 class="text-3xl underline">Just Tauri Todo</h1>
+    <span class="text-gray-500">a simple todo app</span>
+    <div class="mt-3 flex-row border-b-gray-200 border-b-2">
+      <input type="text" class="w-[80vw] max-w-[280px] rounded pl-1 pr-1" style="outline:medium;"
+             placeholder="Press Enter to add a new todo"
              v-model="todoContentInput"
              @keydown.enter="addTodo()">
     </div>
-    <div class="mt-2">
-      <div v-for="(item, index) in todoList" :key="index" class="w-[80vw] max-w-[280px] relative">
-        <input type="checkbox" v-model="item.done" class="ml-2 mr-2">
+
+    <div class="mt-2 ">
+      <div v-for="(item, index) in todoList" :key="index" class="w-[80vw] max-w-[280px] relative p-1 m-[2px] rounded border-2">
+        <input type="checkbox" v-model="item.done" class="ml-2 mr-2" @change="updateTodoDoneStatus(index,item.done)">
         <span :class="item.done ? 'line-through text-gray-500' : ''"
               class="transition-all duration-500 ease-in-out absolute max-w-[200px] break-words">{{ item.text }}</span>
         <span
