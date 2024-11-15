6af98b3dc90ee63380d6c6aed60335a3a924e9cb
erduotong <3135351980@qq.com>
2024-11-15 21:37:27 +0800

    something

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,45 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::collections::VecDeque;
+use std::sync::Mutex;
+#[derive!(clone,serialize,Deserialize)]
+struct TodoItem {
+    content: String,
+    done: bool,
+}
+
+#[derive!(clone,serialize,Deserialize)]
+struct TodoList {
+    items: VecDeque<TodoItem>,
+}
+
+impl TodoList {
+    fn add(&mut self, content: String) {
+        self.items.push_front(TodoItem { content, done: false });
+    }
+    fn remove(&mut self, index: i64) {
+        self.items.remove(index as usize).unwrap();
+    }
+}
+struct TodoListWrapper(pub Mutex<TodoList>);
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn add_todo(content: String) {
+    println!("Adding todo: {}", content);
+}
+#[tauri::command]
+fn remove_todo(index: i64) {
+    println!("Removing todo at index: {}", index);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let todo_list = TodoListWrapper(Mutex::new(TodoList {
+        items: VecDeque::new(),
+    }));
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(todo_list)
+        .invoke_handler(tauri::generate_handler![add_todo,remove_todo])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
